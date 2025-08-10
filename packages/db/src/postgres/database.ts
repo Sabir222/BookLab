4e2808f6c8d552978ad14c0a9daff0a7622943a0
sabir222 <mrsabir4@gmail.com>
2025-08-10 01:43:08 +0100

    feat: filter queries and route

--- packages/db/src/postgres/database.ts
+++ packages/db/src/postgres/database.ts
@@ -1,5 +1,4 @@
 export { db, getConnection, testConnection } from "./client.js";
-export { createTables } from "./schema/index.js";
 export { bookQueries } from "./queries/books.js";
 export { userQueries } from "./queries/users.js";
 export * from "./types/user-types.js";
