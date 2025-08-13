154048b3602ac29e19cb89e69da65d21c5078709
sabir222 <mrsabir4@gmail.com>
2025-08-13 15:11:38 +0100

    feat: 🔍 wishlist

--- packages/db/src/postgres/database.ts
+++ packages/db/src/postgres/database.ts
@@ -2,6 +2,8 @@ export { db, getConnection, testConnection } from "./client.js";
 export { bookQueries } from "./queries/books.js";
 export { userQueries } from "./queries/users.js";
 export { newsletterQueries } from "./queries/newsletter.js";
+export { wishlistQueries } from "./queries/wishlist.js";
 export * from "./types/user-types.js";
 export * from "./types/book-types.js";
 export * from "./types/newsletter-types.js";
+export * from "./types/wishlist-types.js";
