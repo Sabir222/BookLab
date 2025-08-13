b3eb26aeb36ab14be0f19e96c6b62b75d2e7620f
sabir222 <mrsabir4@gmail.com>
2025-08-13 02:14:23 +0100

    feat: carousel

--- apps/web/components/authors/BrowseAuthorsSection.tsx
+++ apps/web/components/authors/BrowseAuthorsSection.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
--- apps/web/components/books/BookFilters.tsx
+++ apps/web/components/books/BookFilters.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Slider } from "@/components/ui/slider";
