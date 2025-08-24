4a0197c7a6a07a2631666496e5418b14c9886226
sabir222 <mrsabir4@gmail.com>
2025-08-24 01:39:22 +0100

    del: deleting unused features

--- packages/db/src/postgres/database.ts
+++ packages/db/src/postgres/database.ts
@@ -1,5 +1,3 @@
 export { db, getConnection, testConnection } from "./client.js";
 export { bookQueries } from "./queries/books.js";
 export { userQueries } from "./queries/users.js";
-export { newsletterQueries } from "./queries/newsletter.js";
-export { wishlistQueries } from "./queries/wishlist.js";
